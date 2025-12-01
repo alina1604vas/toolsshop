12e7af70cb0b60f71d2e39d4658eb7e464780038
Alina_Rubanska <alina1604vas@gmail.com>
2025-12-01 16:11:44 +0200

    Added methods to set fields in CheckoutNameAddressPage

--- src/main/java/org/example/tools/pageobject/CheckoutNameAddressPage.java
+++ src/main/java/org/example/tools/pageobject/CheckoutNameAddressPage.java
@@ -1,37 +1,42 @@
 package org.example.tools.pageobject;
 
+import jdk.incubator.vector.VectorOperators;
 import org.example.tools.SystemConfig;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.example.tools.utils.TestData;
 
 public class CheckoutNameAddressPage {
     private final String url = SystemConfig.getBaseUrl() + "checkout";
     private WebDriver driver;
 
-    @FindBy (id = "first_name")
+    @FindBy(id = "first_name")
     private WebElement firstName;
 
-    @FindBy (id = "email")
+    @FindBy(id = "last_name")
+    private WebElement lastName;
+
+    @FindBy(id = "email")
     private WebElement email;
 
-    @FindBy (id = "address")
+    @FindBy(id = "address")
     private WebElement address;
 
-    @FindBy (id = "city")
+    @FindBy(id = "city")
     private WebElement city;
 
-    @FindBy (id = "state")
+    @FindBy(id = "state")
     private WebElement state;
 
-    @FindBy (id = "country")
+    @FindBy(id = "country")
     private WebElement country;
 
-    @FindBy (id = "postcode")
+    @FindBy(id = "postcode")
     private WebElement postcode;
 
-    public CheckoutNameAddressPage (WebDriver driver) {
+    public CheckoutNameAddressPage(WebDriver driver) {
         this.driver = driver;
     }
 
@@ -45,4 +50,35 @@ public class CheckoutNameAddressPage {
 //
 //    }
 
+    public void enterFirstName() {
+        firstName.sendKeys(TestData.validFirstName());
     }
+
+    public void enterLastName() {
+        lastName.sendKeys(TestData.validLastName());
+    }
+
+    public void enterEmail() {
+        email.sendKeys(TestData.validEmail());
+    }
+
+    public void enterAddress() {
+        address.sendKeys(TestData.validAddress());
+    }
+
+    public void enterCity() {
+        city.sendKeys(TestData.validCity());
+    }
+
+    public void enterState() {
+        state.sendKeys(TestData.validState());
+    }
+
+    public void enterCountry() {
+        country.sendKeys(TestData.validCountry());
+    }
+
+    public void enterPostCode() {
+        postcode.sendKeys(TestData.validPostCode());
+    }
+}
---
+++ src/test/java/org/example/tools/tests/CheckoutNameAddressTest.java
@@ -0,0 +1,5 @@
+package org.example.tools.tests;
+
+public class CheckoutNameAddressTest {
+
+}
