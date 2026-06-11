c3dbbd44f96a675dce90a3b276d50f940dc3df2c
Alina_Rubanska <alina1604vas@gmail.com>
2026-06-11 14:38:11 +0300

    Created ProfileUpdateTest class for testing of profile update functionality.

--- src/test/java/org/example/tools/tests/ProfileUpdateTest.java
+++ src/test/java/org/example/tools/tests/ProfileUpdateTest.java
@@ -1,4 +1,82 @@
 package org.example.tools.tests;
 
+import org.example.tools.SystemConfig;
+import org.example.tools.pageobject.AccountPage;
+import org.example.tools.pageobject.LoginPage;
+import org.example.tools.pageobject.ProfilePage;
+import org.example.tools.pageobject.RegistrationPage;
+import org.example.tools.utils.Customer;
+import org.example.tools.utils.TestData;
+import org.example.tools.utils.UserFactory;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.JavascriptExecutor;
+
+import static org.example.tools.tests.BaseTest.driver;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@TestInstance(TestInstance.Lifecycle.PER_METHOD)
 public class ProfileUpdateTest {
+    LoginPage loginPage;
+    RegistrationPage registrationPage;
+    Customer customer;
+    UserFactory userFactory;
+    ProfilePage profilePage;
+
+    @BeforeEach
+    void setUp() {
+        driver.get(SystemConfig.getBaseUrl());
+        driver.manage().deleteAllCookies();
+        ((JavascriptExecutor) driver).executeScript(
+                "window.localStorage.clear(); window.sessionStorage.clear();"
+        );
+        driver.navigate().refresh();
+
+        registrationPage = new RegistrationPage(driver);
+        userFactory = new UserFactory();
+        registrationPage.open();
+        String selectedCountry = registrationPage.chooseRandomCountry(registrationPage.getAvailableCountries());
+        customer = userFactory.createCustomer(selectedCountry);
+        registrationPage.registerUser(customer);
+        assertTrue(registrationPage.isRegistrationSuccessful(), "Customer was not registered");
+        loginPage = new LoginPage(driver);
+        loginPage.openLogin();
+        AccountPage accountPage = loginPage.logIn(customer.getEmail(), customer.getPassword());
+        assertTrue(accountPage.isPageLoaded(), "Login failed — account page not loaded");
+        profilePage = new ProfilePage(driver).openProfilePage();
+        profilePage.waitUntilPageIsLoaded();
+
+    }
+
+        @AfterEach
+    void tearDown() {
+        loginPage = null;
+        registrationPage = null;
+        profilePage = null;
+    }
+    @Test
+    @DisplayName("Profile Info can be successfully updated")
+    public void profile_CanBeUpdated() {
+        //remove hardcoded country
+        Customer updatedCustomer = userFactory.createCustomer("Ukraine");
+        profilePage.updateProfile(updatedCustomer)
+                .clickUpdateProfileBtn();
+        assertEquals(updatedCustomer.getFirstName(), profilePage.getFirstName());
+        assertEquals(updatedCustomer.getLastName(), profilePage.getLastName());
+        assertEquals(updatedCustomer.getPhone(), profilePage.getPhone());
+        assertEquals(updatedCustomer.getBillingAddress().getStreetAddress(), profilePage.getStreet());
+        assertEquals(updatedCustomer.getBillingAddress().getPostCode(), profilePage.getPostCode());
+        assertEquals(updatedCustomer.getBillingAddress().getCity(), profilePage.getCity());
+        assertEquals(updatedCustomer.getBillingAddress().getState(), profilePage.getState());
+        assertEquals(updatedCustomer.getBillingAddress().getCountry(), profilePage.getCountry());
+        assertEquals("Your profile is successfully updated!", profilePage.getSuccessMsg());
+    }
+    @Test
+    @DisplayName("Password can be successfully changed")
+    public void password_CanBeUpdated () {
+        String newPassword = TestData.validPassword();
+        profilePage.changePassword(customer.getPassword(), newPassword);
+        assertEquals("Your password is successfully updated!", profilePage.getSuccessMsg());
+    }
+
 }
