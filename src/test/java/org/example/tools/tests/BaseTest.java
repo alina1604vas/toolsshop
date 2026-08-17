c46c5f6e59301c73af30b70404984fd68ebe14ca
Alina_Rubanska <alina1604vas@gmail.com>
2026-08-17 17:00:01 +0300

    Add driver lifecycle management ot BaseTest.

--- src/test/java/org/example/tools/tests/BaseTest.java
+++ src/test/java/org/example/tools/tests/BaseTest.java
@@ -1,21 +1,40 @@
 package org.example.tools.tests;
 
 import net.datafaker.Faker;
-import org.example.tools.driver.DriverSingleton;
+import org.example.tools.driver.DriverProvider;
 import org.example.tools.infra.SprintCondition;
 import org.example.tools.network.ChromeResponseListener;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.devtools.DevTools;
 import org.openqa.selenium.devtools.HasDevTools;
-
 @ExtendWith(SprintCondition.class)
 public abstract class BaseTest {
 
-    protected static WebDriver driver = DriverSingleton.getDriver();
-    private static final DevTools devTools = ((HasDevTools) driver).getDevTools();
+    protected WebDriver driver;
+    private DevTools devTools;
 
-    protected static final ChromeResponseListener responseListener = new ChromeResponseListener(devTools);
+    protected ChromeResponseListener responseListener;
     protected final Faker faker = new Faker();
+
+    @BeforeEach
+    void initDriver() {
+        driver = DriverProvider.get();
+        devTools = ((HasDevTools) driver).getDevTools();
+        responseListener = new ChromeResponseListener(devTools);
+    }
+
+    @AfterEach
+    void tearDownDriver() {
+        if (responseListener != null) {
+            responseListener.destroy();
+        }
+        DriverProvider.remove();
+        driver = null;
+        devTools = null;
+        responseListener = null;
+    }
 
 }
