0e5cd093b5f2b0920e8d4a9fe465b7a2454ea88d
Alina_Rubanska <alina1604vas@gmail.com>
2026-08-18 11:31:56 +0300

    Remove getSelectedSprint method and change base url.

--- src/main/java/org/example/tools/SystemConfig.java
+++ src/main/java/org/example/tools/SystemConfig.java
@@ -2,12 +2,8 @@ package org.example.tools;
 
 public class SystemConfig {
 
-    public static int getSelectedSprint() {
-        return Integer.parseInt(System.getProperty("selectedSprint", "0"));
-    }
-
     public static String getBaseUrl() {
-        return System.getProperty("baseUrl", "https://practicesoftwaretesting.com/#");
+        return "https://practicesoftwaretesting.com/";
     }
 
 }
