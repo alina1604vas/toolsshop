a46603f61ba600187f0faff94c4a0b9d1005ec25
Alina_Rubanska <alina1604vas@gmail.com>
2026-08-24 14:24:20 +0300

    Add wait to isHomeVisible method. Change constructor for Header

--- src/main/java/org/example/tools/pageobject/Header.java
+++ src/main/java/org/example/tools/pageobject/Header.java
@@ -3,12 +3,18 @@ package org.example.tools.pageobject;
 import org.example.tools.SystemConfig;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class Header {
 
-    private final String url = SystemConfig.getBaseUrl();
+//    private final String url = SystemConfig.getBaseUrl();
+private final WebDriver driver;
 
     @FindBy(linkText = "Home")
     private WebElement homeNavigationItem;
@@ -19,14 +25,21 @@ public class Header {
     @FindBy(linkText = "Contact")
     private WebElement contactNavigationItem;
 
-    public Header(WebDriver driver) {
-        driver.get(url);
-        PageFactory.initElements(driver, this);
-    }
+//    public Header(WebDriver driver) {
+//        driver.get(url);
+//        PageFactory.initElements(driver, this);
+//    }
+public Header(WebDriver driver) {
+    PageFactory.initElements(driver, this);
+}
 
-    public boolean isHomeVisible() {
-        return homeNavigationItem.isDisplayed();
-    }
+//    public boolean isHomeVisible() {
+//        return homeNavigationItem.isDisplayed();
+//    }
+public boolean isHomeVisible() {
+    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    return wait.until(ExpectedConditions.visibilityOf(homeNavigationItem)).isDisplayed();
+}
 
     public boolean isCategoriesVisible() {
         return categoriesNavigationItem.isDisplayed();
